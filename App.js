04e9af2902736247fedc083b630784c3d666e7ba
euanlimzx <euanlimzx@gmail.com>
2022-11-23 10:08:18 +0800

    firebase installation, before lecture 191

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 //FOR JAVASCRIPT
 //note to self: parenthesis are used as shothand to return an object : ({name:amanda}) is equivalent to { return {name:amanda}}
 //parenthesis can also be used to group multitline return statements
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD6HpoFMouimCJhuOb-U66Pv7vaYY5zd4s",
+  authDomain: "nommy-50094.firebaseapp.com",
+  projectId: "nommy-50094",
+  storageBucket: "nommy-50094.appspot.com",
+  messagingSenderId: "682404579046",
+  appId: "1:682404579046:web:9efa09f1f755397f529d27",
+  measurementId: "G-GV17J6910P",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 import React from "react";
 
 import { ThemeProvider } from "styled-components/native";
